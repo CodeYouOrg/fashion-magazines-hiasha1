cc92168d495aeb5ec4fe7edc966ad71966c5231c
hiasha1 <95436084+hiasha1@users.noreply.github.com>
2024-10-24 20:17:59 -0400

    file added

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,33 @@
+
 -- Add your sql here
+
+SELECT customers.customer_name as Customer,printf('$%.2f',SUM(subscriptions.subscription_length*subscriptions.price_per_month)) AS Amount_Due
+FROM orders
+LEFT JOIN customers ON orders.customer_id = customers.customer_id
+LEFT JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
+WHERE orders.order_status = 'unpaid' and subscriptions.description = 'Fashion Magazine'
+GROUP  BY customers.customer_name ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
